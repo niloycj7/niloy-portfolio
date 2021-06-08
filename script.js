2e1f214f5d9c0cacfcba20422f8dd6893ef31a94
niloycj7 <niloyrahman31@gmail.com>
2021-06-08 20:19:17 +0600

    project added

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ function sendMail(params){
         message: document.getElementById("message").value,
 
     };
-    emailjs.send('service_qi0wg6r', 'template_nlvgqdm', tempParams)
-    .then(function(res){
-        console.log("success", res.status);
-    })
+    // emailjs.send('service_qi0wg6r', 'template_nlvgqdm', tempParams)
+    // .then(function(res){
+    //     console.log("success", res.status);
+    // })
+    emailjs.sendForm('service_qi0wg6r', 'template_nlvgqdm', e.target, 'user_hq45Kda3XJzmTQQ06qP2e')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
 }
